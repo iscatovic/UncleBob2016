649974ba996544226833688574cb08ea74614e52
iscatovic <isaninic@gmail.com>
2016-03-07 22:40:31 +0000

    trying to check if merges okay

--- HuntTheWumpus/src/com/smbcgroup/common/TestClass.java
+++ HuntTheWumpus/src/com/smbcgroup/common/TestClass.java
@@ -3,7 +3,7 @@ package com.smbcgroup.common;
 public class TestClass {
 
 	public static void testSomething() {
-		System.out.println("No");
+		System.out.println("No again?");
 	}
 	
 	public static void main(String args[])
