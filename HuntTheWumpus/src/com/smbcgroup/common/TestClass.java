7edb88b3bd9840e9ff7d0c9b5c29edc9e05b8941
iscatovic <isaninic@gmail.com>
2016-03-07 23:05:36 +0000

    trying to check if pushing after conflict

--- HuntTheWumpus/src/com/smbcgroup/common/TestClass.java
+++ HuntTheWumpus/src/com/smbcgroup/common/TestClass.java
@@ -3,7 +3,7 @@ package com.smbcgroup.common;
 public class TestClass {
 
 	public static void testSomething() {
-		System.out.println("No again?");
+		System.out.println("Back to yes again");
 	}
 	
 	public static void main(String args[])
