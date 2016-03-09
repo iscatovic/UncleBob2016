15cfc81e7cc3dd6096cc93789630106e6ee0cc49
iscatovic <isaninic@gmail.com>
2016-03-09 14:32:10 +0000

    formatted testcontext

--- HTW/test/htw/fixtures/TestContext.java
+++ HTW/test/htw/fixtures/TestContext.java
@@ -10,101 +10,101 @@ import java.util.Map;
 import java.util.Set;
 
 public class TestContext implements HtwMessageReceiver {
-  public static TestableHuntTheWumpus game;
-  public static Set<String> messages = new HashSet<>();
+	public static TestableHuntTheWumpus game;
+	public static Set<String> messages = new HashSet<>();
 
-  public static Map<String, Integer> wumpusCaverns = new HashMap<>();
+	public static Map<String, Integer> wumpusCaverns = new HashMap<>();
 
-  public static Map<String, Integer> batTransportCaverns = new HashMap<>();
+	public static Map<String, Integer> batTransportCaverns = new HashMap<>();
 
-  public TestContext() {
-    game = (TestableHuntTheWumpus) HtwFactory.makeGame("htw.fixtures.TestableHuntTheWumpus", this);
-    messages.clear();
-    wumpusCaverns.clear();
-    batTransportCaverns.clear();
-  }
+	public TestContext() {
+		game = (TestableHuntTheWumpus) HtwFactory.makeGame("htw.fixtures.TestableHuntTheWumpus", this);
+		messages.clear();
+		wumpusCaverns.clear();
+		batTransportCaverns.clear();
+	}
 
-  public void fellInPit() {
-    messages.add("FELL_IN_PIT");
-  }
+	public void fellInPit() {
+		messages.add("FELL_IN_PIT");
+	}
 
-  public void playerMovesToWumpus() {
-    messages.add("PLAYER_MOVES_TO_WUMPUS");
-  }
+	public void playerMovesToWumpus() {
+		messages.add("PLAYER_MOVES_TO_WUMPUS");
+	}
 
-  public void batsTransport() {
-    messages.add("BAT_TRANSPORT");
-  }
+	public void batsTransport() {
+		messages.add("BAT_TRANSPORT");
+	}
 
-  public void wumpusMovesToPlayer() {
-    messages.add("WUMPUS_MOVES_TO_PLAYER");
-  }
+	public void wumpusMovesToPlayer() {
+		messages.add("WUMPUS_MOVES_TO_PLAYER");
+	}
 
-  public void noPassage() {
-    messages.add("NO_PASSAGE");
-  }
+	public void noPassage() {
+		messages.add("NO_PASSAGE");
+	}
 
-  public void hearBats() {
-    messages.add("HEAR_BATS");
-  }
+	public void hearBats() {
+		messages.add("HEAR_BATS");
+	}
 
-  public void hearPit() {
-    messages.add("HEAR_PIT");
-  }
+	public void hearPit() {
+		messages.add("HEAR_PIT");
+	}
 
-  public void smellWumpus() {
-    messages.add("SMELL_WUMPUS");
-  }
-  
-  @Override
-  public void hearElixir() {
-  	messages.add("HEAR_ELIXIR");
-  	
-  }
+	public void smellWumpus() {
+		messages.add("SMELL_WUMPUS");
+	}
 
-  public void arrowsFound(Integer arrowsFound) {
-    messages.add(String.format("%d_ARROW_FOUND", arrowsFound));
-  }
+	@Override
+	public void hearElixir() {
+		messages.add("HEAR_ELIXIR");
 
-  public void passage(HuntTheWumpus.Direction direction) {
-    messages.add(direction.name() + "_PASSAGE");
-  }
+	}
 
-  public void noArrows() {
-    messages.add("NO_ARROWS");
-  }
+	public void arrowsFound(Integer arrowsFound) {
+		messages.add(String.format("%d_ARROW_FOUND", arrowsFound));
+	}
 
-  public void arrowShot() {
-    messages.add("ARROW_SHOT");
-  }
+	public void passage(HuntTheWumpus.Direction direction) {
+		messages.add(direction.name() + "_PASSAGE");
+	}
 
-  public void playerShootsSelfInBack() {
-    messages.add("PLAYER_SHOOTS_SELF_IN_BACK");
-  }
+	public void noArrows() {
+		messages.add("NO_ARROWS");
+	}
 
-  public void playerKillsWumpus() {
-    messages.add("WUMPUS_KILLED");
-  }
+	public void arrowShot() {
+		messages.add("ARROW_SHOT");
+	}
 
-  public void playerShootsWall() {
-    messages.add("PLAYER_SHOOTS_WALL");
-  }
+	public void playerShootsSelfInBack() {
+		messages.add("PLAYER_SHOOTS_SELF_IN_BACK");
+	}
 
-@Override
-public void playerHealed() {
-	messages.add("PLAYER_HEALED");
-	
-}
+	public void playerKillsWumpus() {
+		messages.add("WUMPUS_KILLED");
+	}
 
-@Override
-public void noElixir() {
-	messages.add("NO_ELIXIR");
-	
-}
+	public void playerShootsWall() {
+		messages.add("PLAYER_SHOOTS_WALL");
+	}
 
-@Override
-public void elixirFound() {
-	messages.add("FOUND_ELIXIR");
-	
-}
+	@Override
+	public void playerHealed() {
+		messages.add("PLAYER_HEALED");
+
+	}
+
+	@Override
+	public void noElixir() {
+		messages.add("NO_ELIXIR");
+
+	}
+
+	@Override
+	public void elixirFound() {
+		messages.add("FOUND_ELIXIR");
+
+	}
 }
