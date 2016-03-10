961506ccffce69c44ad04a6a8828f0a50512ba84
Kovey <kcoles@users.noreply.github.com>
2016-03-10 10:05:20 +0000

    Testing package

---
+++ HTW/test/htw/unittests/ElixirTest.java
@@ -0,0 +1,31 @@
+//package htw.unittests;
+//
+//import htw.HuntTheWumpus;
+//import htw.console.Main;
+//import htw.factory.HtwFactory;
+//import htw.fixtures.TestContext;
+//import htw.game.HuntTheWumpusGame;
+//
+//public class ElixirTest extends TestContext{
+//
+//	public void setup() {
+//		
+//	}
+//	public void findElixir()
+//	{
+//		
+//		assert(messages.remove("FOUND_ELIXIR"));
+//		assert(messages.isEmpty());
+//		assert(game.getItems().hasElixir());
+//	}
+//	
+//	public void hasElixirUseElixir()
+//	{
+//		
+//	}
+//	
+//	public void noElixirUseElixir()
+//	{
+//		
+//	}
+//}
---
+++ HTW/test/htw/unittests/GameModeTest.java
@@ -0,0 +1,22 @@
+package htw.unittests;
+
+import htw.HuntTheWumpus;
+import htw.console.Main;
+import htw.factory.HtwFactory;
+import htw.fixtures.TestContext;
+import htw.game.HuntTheWumpusGame;
+
+public class GameModeTest extends TestContext{
+
+	private HuntTheWumpus game =  HtwFactory.makeGame("htw.game.HuntTheWumpusGame", this);	
+	public void characterNamesTest() {
+		
+		
+	}
+	public void gameModeTest() 
+	{
+		
+	}
+	
+	
+}
